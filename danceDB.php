622c07abea9c7b4de18cf06d00ac40cba8aeb053
TakeshiYanagita <LastSamurai297@gmail.com>
2015-06-01 11:19:03 -0600

    changed to use openshift env variable

--- danceDB.php
+++ danceDB.php
@@ -15,7 +15,16 @@
 			$message = "Type your username and password to login <br/>";
 			$passCheck = false;
 			$userCheck = false;
-			$db = new PDO('mysql:host=localhost; dbname=dance; charset=utf8', 'php', 'php-pass');
+			
+			define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+			define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+			define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+			define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+			$dbname = "dance";
+
+			dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
+			$db = new PDO($dsn, DB_USER, DB_PASS);			
+//			$db = new PDO('mysql:host=localhost; dbname=dance; charset=utf8', 'php', 'php-pass');
 			
 			if (!empty($_POST['username']))
 			{
