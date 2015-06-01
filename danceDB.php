d22767bcfe8b493b3caf3fea518f9bb69295d143
TakeshiYanagita <LastSamurai297@gmail.com>
2015-06-01 12:52:52 -0600

    fixed the variable name

--- danceDB.php
+++ danceDB.php
@@ -16,13 +16,13 @@
 			$passCheck = false;
 			$userCheck = false;
 			
-			define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+			define('DB_HOST',getenv('OPENSHIFT_MYSQL_DB_HOST'));
 			define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 			define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 			define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 			$dbname = "dance";
 
-			dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
+			$dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
 			$db = new PDO($dsn, DB_USER, DB_PASS);			
 //			$db = new PDO('mysql:host=localhost; dbname=dance; charset=utf8', 'php', 'php-pass');
 			
