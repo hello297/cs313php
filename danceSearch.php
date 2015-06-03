b0a2edac5a381efc47682aae1618aacaa6b45dab
TakeshiYanagita <LastSamurai297@gmail.com>
2015-06-03 10:29:37 -0600

    Added the search function.  Run test 2

--- danceSearch.php
+++ danceSearch.php
@@ -2,13 +2,13 @@
 	include 'db.php';
 	session_start();
 	$search = "%{$_POST['search']}%";
-	echo $_SESSION['user'];
 	if (isset($_SESSION['user']))
 	{
-		echo $_SESSION['user'];
+		echo $_SESSION['user'] . "<br/>";
+		echo $search . "<br/>";
 		$query = $db->prepare('select * from learned_moves inner join moves on (moves.id = learned_moves.move_id and learned_moves.user_id = :user) where description like :search or move_name like :search');
 		$query->bindParam(':user', $_SESSION['user']);
-		//$query->bindParam(':search', $search);
+		$query->bindParam(':search', $search);
 		$query->execute();
 		if (!$row = $query->fetch())
 		{
